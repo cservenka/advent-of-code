21559fbdf523371696ea16a062303a7967603a09
Martin Holm Cservenka <mhcservenka@gmail.com>
2021-12-01 23:22:23 -0500

    part2

--- 2021/day1.js
+++ 2021/day1.js
@@ -8,11 +8,31 @@ const data = fs
 
 // const data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-let count = 0;
-let last = Number.MAX_SAFE_INTEGER;
-for (const num of data) {
-  if (num > last) count++;
-  last = num;
+function part1(input) {
+  let count = 0;
+  let last = Number.MAX_SAFE_INTEGER;
+  for (const num of input) {
+    if (num > last) count++;
+    last = num;
+  }
+
+  console.log(count);
+  return count;
 }
 
-console.log(count);
+// part1(data);
+
+function part2() {
+  let sums = [];
+
+  for (const [i, num] of data.entries()) {
+    for (let j = 2; j > -1; j--) {
+      if (i - j < 0) continue;
+      if (!sums[i - j]) sums[i - j] = 0;
+      sums[i - j] += num;
+    }
+  }
+
+  part1(sums);
+}
+part2();
